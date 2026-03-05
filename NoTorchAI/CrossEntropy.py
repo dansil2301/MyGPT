79aa60feaa59149d502cd40be7ce73e41be92925
Danila Solovenko <dansil2301@gmail.com>
2026-03-05 22:10:46 +0100

    feat: add Cross Entropy Function

---
+++ NoTorchAI/CrossEntropy.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+from NoTorchAI.ActivationFunc import Softmax
+
+
+class CrossEntropy:
+    def __init__(self):
+        self.softmax = Softmax()
+        self.p_logits = None
+        self.targets = None
+
+    def forward(self, logits: np.ndarray, targets: np.ndarray):
+        B, T, V = logits.shape
+
+        self.targets = targets
+        self.p_logits = self.softmax.forward(logits)
+
+        p_correct = self.p_logits[
+            np.arange(B)[:, None],
+            np.arange(T)[None, :],
+            targets
+        ]
+
+        L = -np.mean(np.log(p_correct))
+        return L
+
+    def backward(self):
+        B, T, V = self.p_logits.shape
+
+        grad = np.copy(self.p_logits)
+
+        grad[
+            np.arange(B)[:, None],
+            np.arange(T)[None, :],
+            self.targets
+        ] -= 1
+
+        grad /= (B * T)
+
+        return grad
