e8e3768b7145379cf6edd1de72ac6dd6bf371961
Danila Solovenko <dansil2301@gmail.com>
2026-03-13 01:36:50 +0100

    feat: initial loading of the model state is ready

--- NoTorchAI/Neuron.py
+++ NoTorchAI/Neuron.py
@@ -24,13 +24,49 @@ class Neuron:
                 saved[c_cls_name][attr_name] = self.__dict__[attr_name]
 
         return saved
+    
+    def _find_implementation(self, name: str) -> type:
+        subclasses = Neuron.__subclasses__()
+        for subclass in subclasses:
+            if subclass.__name__ == str(name):
+                return subclass
+        return None
+
+    def _load(self, saved: dict, c_class_impl: object = None):
+        # todo: add list check. Some lists contain Neurons 
+        for el in saved:
+            print(el, saved[el])
+            # check if the dict is an element or a folded Neuron
+            if isinstance(saved[el], dict):
+                class_name = list(saved[el].keys())[0]
+                attrs = saved[el][class_name]
+                impl_class = self._find_implementation(class_name)
+                if impl_class:
+                    instance = impl_class.__new__(impl_class)
+                    self._load(attrs, instance)
+                    setattr(c_class_impl, el, instance)
+            # if not Neuron just save the value to the variable
+            else:
+                setattr(c_class_impl, el, saved[el])
+        
+        return c_class_impl
 
     def save(self, path: str) -> None:
         saved = self._save()
         np.savez(path + ".npz", saved)
 
-    def load(self, path: str) -> None:
-        saved = np.load(path + ".npz", allow_pickle=True)
-        print(saved.files)
-        for key in saved.files:
-            print(saved[key])
+    def load(self, path: str):
+        archive = np.load(path + ".npz", allow_pickle=True)
+
+        saved = archive["arr_0"]
+        if isinstance(saved, np.ndarray) and saved.dtype == object:
+            saved = saved.item()
+        print(saved)
+        print()
+        
+        # create instance of the saved class and load into it
+        init_name = list(saved.keys())[0]
+        init_class = self._find_implementation(init_name)
+        instance = init_class.__new__(init_class)
+        self._load(saved[init_name], instance)
+        return instance
